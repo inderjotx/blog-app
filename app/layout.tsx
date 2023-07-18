39238aa5f98aa0cf8e2e4244c0d00622f6e0ddc9
inderharrysingh <inderjotsingh141@gmail.com>
2023-07-18 22:47:45 +0530

    commit without reason

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Inder Log',
-  description: 'Blog for badass people',
+  description: 'Blog for ass people',
 }
 
 
